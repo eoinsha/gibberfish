1c9cd7b1ecc7742e63065373971a0f30b7e51da5
Eoin Shanaghy <eoin@showpiper.com>
2016-10-19 15:02:59 +0100

    Mux in generated audio and produce final output

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const Async = require('async');
 const RandomColor = require('randomcolor');
-const Request = require('request');
+const Rimraf = require('rimraf');
 const Saytime = require('saytime');
 const StringEscape = require('js-string-escape');
 const textGen = require('./lib/text-gen');
@@ -14,13 +14,18 @@ const text = textGen();
 
 let tempDir;
 
+const finalVidPath = process.argv[2] || 'out.mp4';
+
 Async.waterfall([
-    createTempDir,
-    renderAudio,
-    renderFrames,
-    renderVideoParts,
-    makePlaylist,
-    assembleVideoParts
+  createTempDir,
+  renderAudio,
+  renderFrames,
+  renderVideoParts,
+  makePlaylist,
+  assembleVideoParts,
+  muxAudio,
+  processResult,
+  cleanTempDir
 ], (err, result) => {
   if (err) {
     return log.error('ERROR', err);
@@ -44,7 +49,7 @@ function renderFrames(state, done) {
 function renderFrame(part, id, done) {
   const imgPath = path.join(tempDir, `${id}.png`);
   const text = StringEscape(part.sentence.match(/(([^\W]+\W){1,6})/g).join('\n')) + `\n${id}:${part.timestamp}`;;
-  childProcess.execFile('gm', [ 'convert', '-size', '640x360', `xc:${RandomColor()}`, '-fill', 'black', '-pointsize', '32', '-gravity', 'Center', '-draw',
+  execFile('gm', [ 'convert', '-size', '640x360', `xc:${RandomColor()}`, '-fill', 'black', '-pointsize', '24', '-gravity', 'Center', '-draw',
       `text 0,0 "${text}"`, imgPath ], error => {
     if (error) {
       return done(error);
@@ -64,7 +69,8 @@ function renderVideoParts(state, done) {
 
 function renderVideoPart(part, id, done) {
   const vidPath = path.join(tempDir, `${id}.mp4`);
-  childProcess.execFile('ffmpeg', [ '-loop', 1, '-i', part.imgPath, '-c:v', 'libx264', '-t', part.duration, '-pix_fmt', 'yuv420p', vidPath ], vidError => {
+  const clipLength = part.duration + (part.after || 0);
+  execFile('ffmpeg', [ '-loop', 1, '-i', part.imgPath, '-c:v', 'libx264', '-t', clipLength, '-pix_fmt', 'yuv420p', vidPath ], vidError => {
     if (vidError) {
       return done(vidError);
     }
@@ -88,11 +94,21 @@ function makePlaylist(state, done) {
 
 function assembleVideoParts(state, done) {
   const assembledVidPath = path.join(tempDir, 'assembled.mp4');
-  childProcess.execFile('ffmpeg', [ '-f', 'concat', '-i', state.listFilePath, '-c', 'copy', assembledVidPath ], (error, stdout, stderr) => {
+  execFile('ffmpeg', [ '-f', 'concat', '-i', state.listFilePath, '-c', 'copy', assembledVidPath ], (error, stdout, stderr) => {
     if (error) {
       return done(error);
     }
     return done(null, Object.assign(state, { assembledVidPath }));
+  });
+}
+
+function muxAudio(state, done) {
+  const muxedVidPath = path.join(tempDir, 'muxed.mp4');
+  execFile('ffmpeg', [ '-i', state.assembledVidPath, '-i', state.audioPath, '-c:v', 'copy', '-c:a', 'aac', '-b:a', '48k', '-strict', 'experimental', muxedVidPath ], (error, stdout, stderr) => {
+    if (error) {
+      return done(error);
+    }
+    return done(null, Object.assign(state, { muxedVidPath }));
   });
 }
 
@@ -101,4 +117,19 @@ function createTempDir(cb) {
     tempDir = dir;
     cb(err);
   });
+}
+
+function cleanTempDir(result, cb) {
+  Rimraf(tempDir, err => cb(err, result));
+}
+
+function processResult(state, cb) {
+  fs.rename(state.muxedVidPath, finalVidPath, err => {
+    return cb(err, finalVidPath);
+  });
+}
+
+function execFile(file, args, cb) {
+  log.info('execFile', file, args.join(' '));
+  childProcess.execFile(file, args, cb);
 }
